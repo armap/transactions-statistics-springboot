30a12b3f4ea5dc9741a546f26f6f25a025bfc2e8
arnau <arnaumap@gmail.com>
2021-08-07 21:08:12 +0200

    Add private constructor to Utility class

--- src/main/java/com/n26/util/BigDecimalUtil.java
+++ src/main/java/com/n26/util/BigDecimalUtil.java
@@ -1,8 +1,12 @@
 package com.n26.util;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class BigDecimalUtil {
     public static BigDecimal setCustomScale(BigDecimal bigDecimal) {
         return bigDecimal.setScale(2, RoundingMode.HALF_UP);
